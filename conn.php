75ae4ae65231475c49f517035c2469be7e72616e
sagarcadet <sagarcadet@gmail.com>
2017-04-02 00:06:20 +0530

    added a message for successful connection
    just chilling!

--- conn.php
+++ conn.php
@@ -4,5 +4,12 @@ $mysql_username= "root";
 $mysql_password= "";
 $server_name= "localhost";
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
-
+if($conn)
+{
+echo "connection successful";
+}
+else
+{
+echo "not connected"
+}
 ?>
